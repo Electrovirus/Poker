ee891fc69c82e1cf95c20c66cb69acd03d86c5a5
Electrovirus <E.electrovirus@gmail.com>
2016-06-18 02:48:46 +0430

    Update info.lua

--- plugins/info.lua
+++ plugins/info.lua
@@ -1,7 +1,7 @@
 local function run(msg, matches)
 
 if matches[1]:lower() == 'info' then
-return '🔰 Your Name: '..msg.from.print_name..'\n🔰 Your Id: '..msg.from.id..'\n🔰 Yourt User Naem: @'..msg.from.username..'\n🔰 Your Phone Number: '..msg.from.phone..'\n🔰 Your TelegramLink: telegram.me/'..msg.from.username..'\n⚠️ Group Name: '..msg.to.print_name..'\n⚠️ Group Id: '..msg.to.id..'⚠️ Text Messages: '..msg.text
+return '😐 Your Name: '..msg.from.print_name..'\n😐 Your Id: '..msg.from.id..'\n😐 Yourt User Name: @'..msg.from.username..'\n😐 Your Phone Number: '..msg.from.phone..'\n😐 Your TelegramLink: telegram.me/'..msg.from.username..'\n😐 Group Name: '..msg.to.print_name..'\n😐 Group Id: '..msg.to.id..'\n😐 Text Messages: '..msg.text
 end
 end
 
