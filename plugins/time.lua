c1a5c43301e900cfebe2a23dfc37f017b2449870
Electrovirus <E.electrovirus@gmail.com>
2016-06-18 13:31:32 +0430

    Poker

--- plugins/time.lua
+++ plugins/time.lua
@@ -1,12 +1,3 @@
-#addplug --[[
-#
-#    Time And Date
-#
-#    
-#
-#
-]]
-
 function run(msg, matches)
 local url , res = http.request('http://api.gpmod.ir/time/')
 if res ~= 200 then return "No connection" end
